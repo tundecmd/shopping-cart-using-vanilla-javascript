ed2a1d69da72986d17a8c43ae02215441e40221a
tundecmd <tundecmd@gmail.com>
2020-01-27 15:52:42 -0800

    end of lesson 1-intro to array basics

---
+++ main.js
@@ -0,0 +1,18 @@
+/*
+var array = ['Apple','Brush','Carrot']
+console.log(array.length);
+console.log(array);
+console.log(array[1]);
+
+
+array.push('Dice')
+console.log(array.length);
+console.log(array);
+*/
+
+var array = ['Apple','Brush','Carrot']
+
+  array.push('Ducks')
+  for ( var i = 0; i < array.length; i ++) {
+      console.log(array[i])
+  }
