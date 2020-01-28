d618042df551968b690465a8dc004cb8ac200251
tundecmd <tundecmd@gmail.com>
2020-01-28 00:34:24 -0800

    end of lesson 6- CartItem and Cart

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
-// scope determines where you can see your value or variable witnin your program.
+//general structure
 
-var global = 10;
+//var cart = [{name: 'Brush', price: '1.99', count: '1'}, {name, price, count},{name, price, count}];
+// name, price, count
 
-function myFunction() {
-  var global = 'Hello from main.js';
-  console.log('global: ',global);
+
+var cart = [];
+
+// FACTORY FUNCTIONS AND CONSTRUCTOR FUNCTIONS(OR CLASSES) ARE ALTERNATIVES ALTHOUGH CONSTRUCTORS( OR CLASSES) ARE NEWER STANDARD AND ARE THUS RECOMMENDED. FACTORY FUNCTIONS ARE USED IN THIS EXAMPLE JUST FOR DEMONSTRATION PURPOSE 
+
+// FACTORY FUNCTION
+var Item = function (name, price, count) {
+  this.name = name,
+  this.price = price,
+  this.count = count
 }
 
-myFunction()
+// CONSTRUCTOR FUNCTION
+class Itemmm {
+  constructor (name, price, count) {
+    this.name = name,
+    this.price = price,
+    this.count = count
+  }
+}
 
 
-console.log('global: ',global);
+//var a = new Item('Brush', 1.99, 1 )
+
+
+var aaa = new Itemmm('Brushhh', 1.99, 1 )
+//console.log(aaa);
+
+cart.push(new Itemmm('Apple', 5.99, 2 ));
+cart.push(aaa);
+
+console.log(cart);
