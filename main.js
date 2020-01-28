6086654fb0cc51e00473646f32ca364dd4222a8a
tundecmd <tundecmd@gmail.com>
2020-01-28 05:06:06 -0800

    end of lesson 7 - Cart functions

--- main.js
+++ main.js
@@ -9,12 +9,13 @@ var cart = [];
 // FACTORY FUNCTIONS AND CONSTRUCTOR FUNCTIONS(OR CLASSES) ARE ALTERNATIVES ALTHOUGH CONSTRUCTORS( OR CLASSES) ARE NEWER STANDARD AND ARE THUS RECOMMENDED. FACTORY FUNCTIONS ARE USED IN THIS EXAMPLE JUST FOR DEMONSTRATION PURPOSE 
 
 // FACTORY FUNCTION
+/*
 var Item = function (name, price, count) {
   this.name = name,
   this.price = price,
   this.count = count
 }
-
+*/
 // CONSTRUCTOR FUNCTION
 class Itemmm {
   constructor (name, price, count) {
@@ -24,14 +25,12 @@ class Itemmm {
   }
 }
 
+// LIST OF CART FUNCTIONS
 
-//var a = new Item('Brush', 1.99, 1 )
-
-
-var aaa = new Itemmm('Brushhh', 1.99, 1 )
-//console.log(aaa);
-
-cart.push(new Itemmm('Apple', 5.99, 2 ));
-cart.push(aaa);
-
-console.log(cart);
+//addItemToCart(name, price, count)
+//removeItemFromCart(name) -- removes one item
+//removeItemFromCartAll(name) -- removes all of item-name
+//clearCart() -- removes all the items from the cart
+//countCart() -- returns total count
+// totalCart() -- returns total cost
+//listCart() -- array of items
